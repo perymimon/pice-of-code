1638929dfea1bcb6f2ddef2fd2db3eb246e1ef1f
pery mimon <pery.mimon@gmail.com>
2020-03-05 21:14:08 +0200

    add flatObject and objectGet

---
+++ objects.js
@@ -0,0 +1,24 @@
+export function objectGet(obj, path) {
+    const parts = path.split(/\.|\[['"`]|['"`]]/).filter(Boolean);
+    let i = 0, value, key;
+
+    while ((key = parts[i++]) && (obj = obj[key])) ;
+    return obj;
+}
+
+export function flatObject(obj) {
+    const flatObject = {};
+    const path = [];
+
+    function dig(obj) {
+        for (let key in obj) {
+            path.push(key);
+            flatObject[path.join('.')] = dig(obj[key]);
+            path.pop();
+        }
+        return obj;
+    }
+
+    dig(obj);
+    return flatObject;
+}
